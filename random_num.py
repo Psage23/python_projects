89f5c6392eed8a27bd55c21287ee7676df1d216b
Psage23 <pamelasage3@gmail.com>
2021-09-25 19:42:17 -0700

    typo and fixed

--- random_num.py
+++ random_num.py
@@ -33,7 +33,7 @@ def computer_guess(x):
         feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
         if feedback == 'h':
             high = guess - 1
-        if feedback == 'l':
+        elif feedback == 'l':
             low = guess + 1
 
     print(f'Yay! The computer guessed your number, {guess}, correctly!')
